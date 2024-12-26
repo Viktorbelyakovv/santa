aeed0b4aba00bae83ee21273d1564bcdf02b1ff2
Viktor Belyakov <belyakoviktor97@yandex.ru>
2024-12-27 00:31:55 +0400

    Message

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,24 @@
+import styles from './Main.module.scss'
+import classNames from 'classnames';
+const Main = () => {
+
+    const handleLinkClick = (event) => {
+        event.preventDefault();
+        window.open('/docs/ozon.pdf', '_blank', 'noopener,noreferrer');
+      };
+
+    return <div className={styles.main}>
+        <div className={classNames(styles.colorText, styles.font, styles.unselectableText)}>
+            <div>Никита Захарычев,</div>
+            <div>с Новым годом!</div>
+        </div>
+        <div>            
+            <button onClick={handleLinkClick} className={styles.pressMeButton}>                
+                <img className={styles.img} src={"src/assets/gift.gif"}/>
+                </button>
+        </div>
+        <div className={classNames(styles.pressText, styles.colorText,  styles.unselectableText)}>Нажми на подарок!</div>
+    </div>
+}
+
+export default Main;
